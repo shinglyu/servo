e52af91176c3d3013a799a797497344acf73239f
Shing Lyu <shing.lyu@gmail.com>
2016-01-29 17:32:39 +0800

    Run all tests, no update.sh

--- python/servo/testing_commands.py
+++ python/servo/testing_commands.py
@@ -232,6 +232,22 @@ class MachCommands(CommandBase):
     def test_tidy(self, faster):
         return tidy.scan(faster)
 
+    @Command('test-webidl',
+             description='Run the WebIDL parser tests',
+             category='testing')
+    @CommandArgument('--quiet', '-q', default=False, action="store_true",
+                     help="Don't print passing tests.")
+    def test_webidl(self, quiet):
+        self.ensure_bootstrapped()
+
+        test_file_dir = path.abspath(path.join(PROJECT_TOPLEVEL_PATH, "components", "script", "dom", "bindings", "codegen", "parser"))
+        sys.path.insert(0, test_file_dir)
+        run_file = path.abspath(path.join(test_file_dir, "runtests.py"))
+        run_globals = {"__file__": run_file}
+        execfile(run_file, run_globals)
+        verbose = not quiet
+        return run_globals["run_tests"](None, verbose)
+
     @Command('test-wpt-failure',
              description='Run the web platform tests',
              category='testing')
